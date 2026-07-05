e92ea655bde23b2a825541812fd67d4177be6805
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-06 00:04:08 +0700

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <GLFW/glfw3.h>
+
+int main( void ) {
+	GLFWwindow* window;
+
+	if ( !glfwInit() )
+		return EXIT_FAILURE;
+
+	window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
+
+	if ( !window ) {
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent( window );
+
+	while ( !glfwWindowShouldClose( window ) ) {
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		glfwSwapBuffers( window );
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
+}
