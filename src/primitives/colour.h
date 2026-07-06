4b7a0747b9bf0b4e1f44c225d6561dcf14d010be
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-06 23:01:52 +0700

    Removed trailling whitespace

--- src/primitives/colour.h
+++ src/primitives/colour.h
@@ -10,10 +10,10 @@ typedef struct colour {
 	uint8_t a;
 } colour;
 
-#define VINTAGE_GOLD	((colour){ 42, 48, 41, 255 }) 
-#define BLACK		((colour){ 0, 0, 0, 255 }) 
-#define RED		((colour){ 255, 0, 0, 255 }) 
-#define GREEN		((colour){ 0, 255, 0, 255 }) 
-#define BLUE		((colour){ 0, 0, 255, 255 }) 
+#define VINTAGE_GOLD	((colour){ 42, 48, 41, 255 })
+#define BLACK		((colour){ 0, 0, 0, 255 })
+#define RED		((colour){ 255, 0, 0, 255 })
+#define GREEN		((colour){ 0, 255, 0, 255 })
+#define BLUE		((colour){ 0, 0, 255, 255 })
 
 #endif
