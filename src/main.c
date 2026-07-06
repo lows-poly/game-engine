da43809b05be7fcf23736cc1cb3fd7a9048dfdfc
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-06 12:03:50 +0700

    Integrate input and fix window

--- src/main.c
+++ src/main.c
@@ -1,19 +1,30 @@
 #include <stdlib.h>
 
 #include "window/window.h"
+#include "input.h"
 
 int main( void ) {
-	struct window win;
+	struct window_state window;
+	struct input_state input;
 
-	if ( !window_init( &win ) )
+	if ( !window_init( &window ) )
 		return EXIT_FAILURE;
 
-	while ( !window_should_close( &win ) ) {
+	input_init( &input, window.handle );
+
+	while ( !window_should_close( &window ) ) {
+		window_poll_events( &window );
 		glClear( GL_COLOR_BUFFER_BIT );
-		window_swap_and_poll( &win );
+
+		if ( input_key_pressed( &input, KEY_ESCAPE ) )
+			window_set_should_close( &window, true );
+
+		window_swap_buffers( &window );
+		input_update( &input );
 	}
 
-	window_destroy( &win );
+	input_destroy( &input, &window );
+	window_destroy( &window );
 
 	return EXIT_SUCCESS;
 }
