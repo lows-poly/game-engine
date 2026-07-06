f0b0c2dadfdc98040b5400d0ff7dc567ad65556f
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-06 12:02:53 +0700

    Renamed struct "window" to "window_state". Added window_set_should_close()

--- include/window/window.h
+++ include/window/window.h
@@ -12,16 +12,18 @@
 #include <glad.h>
 #include <GLFW/glfw3.h>
 
-struct window {
+struct window_state {
 	GLFWwindow *handle;
 	int width;
 	int height;
 	bool resized;
 };
 
-int window_init( struct window *w );
-int window_should_close( struct window *w );
-void window_swap_and_poll( struct window *w );
-void window_destroy( struct window *w );
+int window_init( struct window_state *w );
+int window_should_close( struct window_state *w );
+void window_set_should_close( struct window_state *w, bool value );
+void window_poll_events( struct window_state *w );
+void window_swap_buffers( struct window_state *w );
+void window_destroy( struct window_state *w );
 
 #endif
