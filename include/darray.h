ec7febee325db701920f71f137d51457f254a405
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-07 21:26:35 +0700

    Finalised darray.h

--- include/darray.h
+++ include/darray.h
@@ -1,24 +1,30 @@
 #ifndef DARRAY_H
 #define DARRAY_H
 
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define darray(T) struct { \
 	T *data; \
 	size_t count; \
-	size_t capacity \
+	size_t capacity; \
 }
 
 #define darray_alloc( arr ) \
-	do { \
+	( { \
+		int _err = 0; \
+		\
 		(arr) = malloc( sizeof( *(arr) ) ); \
-		if ( arr ) { \
+		if ( !(arr) ) { \
+			_err = -ENOMEM; \
+		} else { \
 			(arr)->data = NULL; \
 			(arr)->count = 0; \
 			(arr)->capacity = 0; \
 		} \
-	} while (0)
+		_err; \
+	} )
 
 #define darray_free( arr ) \
 	do { \
@@ -30,9 +36,12 @@
 	} while (0)
 
 #define darray_reserve( arr, n ) \
-	do { \
+	( { \
+		int _err = 0; \
+		\
 		if ( (arr)->capacity < (size_t)(n) ) { \
 			size_t new_cap_; \
+			void *new_data_; \
 			\
 			if ( (arr)->capacity > 0 ) \
 				new_cap_ = (arr)->capacity; \
@@ -42,31 +51,66 @@
 			while ( new_cap_ < (size_t)(n) ) \
 				new_cap_ *= 2; \
 			\
-			void *new_data_ = realloc( (arr)->data, \
-			                           new_cap_ * sizeof( *(arr)->data ) ) \
+			new_data_ = realloc( (arr)->data, \
+			                      new_cap_ * sizeof( *(arr)->data ) ); \
 			\
-			if ( new_data_ ) {\
+			if ( !new_data_ ) { \
+				_err = -ENOMEM; \
+			} else { \
 				(arr)->data = new_data_; \
 				(arr)->capacity = new_cap_; \
 			} \
 		} \
-	} while (0)
+		_err; \
+	} )
 
 #define darray_push( arr, src ) \
-	do { \
-		darray_reserve( (arr), (arr)->count + 1 ); \
-		(arr)->data[(arr)->count++] = (src); \
-	} while (0)
+	( { \
+		int _err = darray_reserve( (arr), (arr)->count + 1 ); \
+		\
+		if ( !_err ) \
+			(arr)->data[(arr)->count++] = (src); \
+		_err; \
+	} )
 
-#define darray_pop( arr ) \
-	( (arr)->data[--(arr)->count] );
+#define darray_pop( arr, out ) \
+	( { \
+		int _err = 0; \
+		\
+		if ( (arr)->count == 0 ) { \
+			_err = -ENOENT; \
+		} else { \
+			(arr)->count--; \
+			*(out) = (arr)->data[(arr)->count]; \
+		} \
+		_err; \
+	} )
 
 #define darray_remove( arr, index ) \
-	do { \
-		memmove( &(arr)->data[index], &(arr)->data[(index) + 1] ), \
-		         (( (arr)->count - (index) - 1 )) * sizeof( *(arr)->data ));\
-		(arr)->count--; \
-	} while (0)
+	( { \
+		int _err = 0; \
+		\
+		if ( (size_t)(index) >= (arr)->count ) { \
+			_err = -ERANGE; \
+		} else { \
+			memmove( &(arr)->data[index], &(arr)->data[(index) + 1], \
+			         ( (arr)->count - (index) - 1 ) * sizeof( *(arr)->data ) ); \
+			(arr)->count--; \
+		} \
+		_err; \
+	} )
+
+#define darray_get( arr, index, out ) \
+	( { \
+		int _err = 0; \
+		\
+		if ( (size_t)(index) >= (arr)->count ) { \
+			_err = -ERANGE; \
+		} else { \
+			*(out) = (arr)->data[index]; \
+		} \
+		_err; \
+	} )
 
 #define darray_clear( arr ) \
 	( (arr)->count = 0 )
