edf755a24a12d4a5303e61e71d8228ae55a23d1e
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-16 22:52:30 +0700

    Functions now return boolean instead of int

--- src/lib/file.c
+++ src/lib/file.c
@@ -4,7 +4,7 @@
 
 #include "path.h"
 
-int file_read_binary( const char *path, void **out_data, size_t *out_size )
+bool file_read_binary( const char *path, void **out_data, size_t *out_size )
 {
 	FILE *fp;
 	long size;
@@ -12,34 +12,34 @@ int file_read_binary( const char *path, void **out_data, size_t *out_size )
 
 	fp = fopen( path, "rb" );
 	if ( fp == NULL )
-		return -1;
+		return false;
 
 	if ( fseek( fp, 0, SEEK_END ) < 0 ) {
 		fclose( fp );
-		return -1;
+		return false;
 	}
 
 	size = ftell( fp );
 	if ( size < 0 ) {
 		fclose( fp );
-		return -1;
+		return false;
 	}
 
 	if ( fseek( fp, 0, SEEK_SET ) < 0 ) {
 		fclose( fp );
-		return -1;
+		return false;
 	}
 
 	buf = malloc( (size_t) size );
 	if ( buf == NULL ) {
 		fclose( fp );
-		return -1;
+		return false;
 	}
 
 	if ( fread( buf, 1, (size_t) size, fp ) != (size_t) size ) {
 		free( buf );
 		fclose( fp );
-		return -1;
+		return false;
 	}
 
 	fclose( fp );
@@ -47,48 +47,48 @@ int file_read_binary( const char *path, void **out_data, size_t *out_size )
 	*out_data = buf;
 	*out_size = (size_t) size;
 
-	return 0;
+	return true;
 }
 
-int file_read_binary_rel( const char *rel_path, void **out_data, size_t *out_size )
+bool file_read_binary_rel( const char *rel_path, void **out_data, size_t *out_size )
 {
 	char full_path[PATH_MAX_LEN];
 
 	if ( path_resolve( rel_path, full_path, sizeof( full_path ) ) < 0 )
-		return -1;
+		return false;
 
 	return file_read_binary( full_path, out_data, out_size );
 }
 
-int file_read_text_rel( const char *rel_path, char **out_data )
+bool file_read_text_rel( const char *rel_path, char **out_data )
 {
 	char full_path[PATH_MAX_LEN];
 
 	if ( path_resolve( rel_path, full_path, sizeof( full_path ) ) < 0 )
-		return -1;
+		return false;
 
 	return file_read_text( full_path, out_data );
 }
 
-int file_read_text( const char *path, char **out_data )
+bool file_read_text( const char *path, char **out_data )
 {
 	void *data;
 	size_t size;
 	char *buf;
 
 	if ( file_read_binary( path, &data, &size ) < 0 )
-		return -1;
+		return false;
 
 	buf = realloc( data, size + 1 );
 	if ( buf == NULL ) {
 		free( data );
-		return -1;
+		return false;
 	}
 
 	buf[size] = '\0';
 	*out_data = buf;
 
-	return 0;
+	return true;
 }
 
 void file_free( void *data )
