bc19ba93250129f6b0cc6442152cd52f5a55cf6b
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-16 22:52:14 +0700

    Added mesh.h

---
+++ src/graphics/mesh.h
@@ -0,0 +1,27 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <glad/glad.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "buffer.h"
+#include "vertex_array.h"
+
+struct mesh {
+	struct vertex_array vao;
+	struct buffer vbo;
+	struct buffer ebo;
+	GLsizei vertex_count;
+	GLsizei index_count;
+	bool has_indices;
+};
+
+int mesh_create( struct mesh *m, const void *vertices, size_t vertex_size,
+                  GLsizei vertex_count, const struct vertex_attrib *attribs,
+                  size_t attrib_count, const unsigned int *indices,
+                  GLsizei index_count );
+void mesh_draw( const struct mesh *m );
+void mesh_destroy( struct mesh *m );
+
+#endif
