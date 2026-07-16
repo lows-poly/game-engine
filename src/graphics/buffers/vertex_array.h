8b7af531c135fd0986e7cb24e8cc7adf9a363af4
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-16 22:51:07 +0700

    Added vertex_array.h

---
+++ src/graphics/buffers/vertex_array.h
@@ -0,0 +1,28 @@
+#ifndef VERTEX_ARRAY_H
+#define VERTEX_ARRAY_H
+
+#include <stdbool.h>
+#include <glad/glad.h>
+#include "buffer.h"
+
+struct vertex_array {
+	GLuint id;
+};
+
+struct vertex_attrib {
+	GLuint index;
+	GLint size;
+	GLenum type;
+	GLboolean normalised;
+	GLsizei stride;
+	size_t offset;
+};
+
+int vertex_array_create( struct vertex_array *va );
+int vertex_array_link_attrib( struct vertex_array *va, const struct buffer *vbo,
+                              struct vertex_attrib *attrib );
+void vertex_array_bind( const struct vertex_array *va );
+void vertex_array_unbind( void );
+void vertex_array_destroy( struct vertex_array *va );
+
+#endif
