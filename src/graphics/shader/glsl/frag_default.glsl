a418cc25d95f9a78fe59c28825340b5330be911f
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-17 00:25:08 +0700

    fragment shader receive vec4 instead of uniform vec4

--- src/graphics/shader/glsl/frag_default.glsl
+++ src/graphics/shader/glsl/frag_default.glsl
@@ -1,10 +1,10 @@
 #version 410 core
 
-uniform vec4 vert_colour;
+in vec4 vert_colour;
 
-out ve4 frag_colour;
+out vec4 frag_colour;
 
 void main()
 {
-	frag_colour = vert_colour
+	frag_colour = vert_colour;
 }
