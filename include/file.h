5842954ad09efdef316ea3e9b1cd483195f3ffff
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-16 22:50:38 +0700

    Functions now return bool instead of int

--- include/file.h
+++ include/file.h
@@ -2,12 +2,13 @@
 #define FILE_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
-int file_read_binary( const char *path, void **out_data, size_t *out_size );
-int file_read_text( const char *path, char **out_data );
+bool file_read_binary( const char *path, void **out_data, size_t *out_size );
+bool file_read_text( const char *path, char **out_data );
 
-int file_read_binary_rel( const char *rel_path, void **out_data, size_t *out_size );
-int file_read_text_rel( const char *rel_path, char **out_data );
+bool file_read_binary_rel( const char *rel_path, void **out_data, size_t *out_size );
+bool file_read_text_rel( const char *rel_path, char **out_data );
 
 void file_free( void *data );
 
