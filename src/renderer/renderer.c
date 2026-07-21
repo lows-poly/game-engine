40b3a287db194465daf9e7f589b648bf21470db1
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-21 12:11:18 +0700

    Reduce boilerplate on OpenG. Added face culling and draw mesh function

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -1,17 +1,27 @@
 #include "renderer.h"
+#include "primitives/colourf.h"
 
-void renderer_clear( GLbitfield mask )
-{
-	glClear( mask );
-}
-
-void renderer_clear_colour( colour c )
+void renderer_begin_frame( colour c )
 {
 	colourf cf = colour_to_float( c );
+
 	glClearColor( cf.r, cf.g, cf.b, cf.a );
+	glClear( GL_COLOR_BUFFER_BIT );
 }
 
-void renderer_clear_colourf( colourf cf )
+void renderer_enable_backface_culling( bool value, enum draw_order order )
 {
-	glClearColor( cf.r, cf.g, cf.b, cf.a );
+	if ( value ) {
+		glEnable( GL_CULL_FACE );
+		glCullFace( GL_BACK );
+		glFrontFace( order );
+	} else {
+		glDisable( GL_CULL_FACE );
+	}
+}
+
+void renderer_draw_mesh( const struct shader *s, const struct mesh *m )
+{
+	shader_use( s );
+	mesh_draw( m );
 }
--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -2,12 +2,19 @@
 #define ENGINE_RENDERER_H
 
 #include <glad/glad.h>
+#include <stdbool.h>
 
+#include "graphics/shader/shader.h"
+#include "graphics/mesh.h"
 #include "primitives/colour.h"
-#include "primitives/colourf.h"
 
-void renderer_clear( GLbitfield mask );
-void renderer_clear_colour( colour c );
-void renderer_clear_colourf( colourf c );
+enum draw_order {
+	DRAW_ORDER_CW = GL_CW,
+	DRAW_ORDER_CCW = GL_CCW
+};
+
+void renderer_begin_frame( colour c );
+void renderer_enable_backface_culling( bool value, enum draw_order order );
+void renderer_draw_mesh( const struct shader *s, const struct mesh *m );
 
 #endif
