55c3de23ca8c44e53a4cb67001d39197d7bf5fd8
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-25 04:52:43 +0700

    Renamed shader and mesh create functions. And removed err_ for window_init

--- src/main.c
+++ src/main.c
@@ -67,24 +67,19 @@ int main( int argc, char *argv[] )
 	int err_;
 
 	if ( path_init( argv[0] ) < 0 )
-		err(EXIT_FAILURE, "FAILED TO INIT PATH");
+		err( EXIT_FAILURE, "FAILED TO INIT PATH" );
 
-	err_ = window_init( &window, WINDOW_WIDTH, WINDOW_HEIGHT,
-	                    WINDOW_TITLE );
-	if ( err_ )
-		err(EXIT_FAILURE, "FAILED TO INIT WINDOW");
-
+	window_init( &window, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE );
 	input_init( &input, window.handle );
 	timer_init( &tm, TARGET_FRAMERATE );
 
-	err_ = shader_create( &shader, "src/graphics/shader/glsl/vert_default.glsl",
-	                      "src/graphics/shader/glsl/frag_default.glsl" );
-	
+	err_ = shader_init( &shader, "src/graphics/shader/glsl/vert_default.glsl",
+	                    "src/graphics/shader/glsl/frag_default.glsl" );
 	if ( err_ )
 		err(EXIT_FAILURE, "FAILED TO CREATE SHADER");
 
-	err_ = mesh_create( &triangle, vertices, sizeof( vertices ), 3, attribs,
-	                    2, NULL, 0 );
+	err_ = mesh_init( &triangle, vertices, sizeof( vertices ), 3, attribs,
+	                  2, NULL, 0 );
 	if ( err_ )
 		err(EXIT_FAILURE, "FAILED TO CREATE MESH");
 
