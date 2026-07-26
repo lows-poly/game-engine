bca7511c0f02530de34a4727feed34ce163c4c78
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-26 22:01:17 +0700

    Add functions comments

--- src/app/app.c
+++ src/app/app.c
@@ -1,5 +1,12 @@
 #include "app.h"
 
+/*
+ * app_init() - Initialise window and input
+ * @app:    app struct
+ * @width:  Window width
+ * @height: Window height
+ * @title:  Window title
+ */
 int app_init( struct app *app, int width, int height, const char *title )
 {
 	if ( !window_init( &app->win, width, height, title ) )
@@ -10,6 +17,10 @@ int app_init( struct app *app, int width, int height, const char *title )
 	return 1;
 }
 
+/*
+ * app_running() - Update input, swap buffers, poll events, and limit fps
+ * @app: app struct
+ */
 int app_running( struct app *app )
 {
 	input_update( &app->input );
@@ -19,11 +30,19 @@ int app_running( struct app *app )
 	return !window_should_close( &app->win );
 }
 
+/*
+ * app_stop() - Set close window
+ * @app: app struct
+ */
 void app_stop( struct app *app )
 {
 	window_set_should_close( &app->win, 1 );
 }
 
+/*
+ * app_shutdown() - Destroy input and window
+ * @app: app struct
+ */
 void app_shutdown( struct app *app )
 {
 	input_destroy( &app->input, &app->win );
--- src/app/app.h
+++ src/app/app.h
@@ -15,9 +15,31 @@ struct app {
 	struct input_state input;
 };
 
+/*
+ * app_init() - Initialise window and input
+ * @app:    app struct
+ * @width:  Window width
+ * @height: Window height
+ * @title:  Window title
+ */
 int app_init( struct app *app, int width, int height, const char *title );
+
+/*
+ * app_running() - Update input, swap buffers, poll events, and limit fps
+ * @app: app struct
+ */
 int app_running( struct app *app );
+
+/*
+ * app_stop() - Set close window
+ * @app: app struct
+ */
 void app_stop( struct app *app );
+
+/*
+ * app_shutdown() - Destroy input and window
+ * @app: app struct
+ */
 void app_shutdown( struct app *app );
 
 #endif
