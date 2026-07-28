0232131c91c6ac8e71f5a7badf6ce28a2d266c58
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-28 23:52:50 +0700

    Add make function and union type

--- src/primitives/colour.h
+++ src/primitives/colour.h
@@ -4,17 +4,36 @@
 #include <stdint.h>
 
 typedef struct colour {
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
+	union {
+		struct { uint8_t r, g, b, a; };
+		uint8_t raw[3];
+	};
 } colour;
 
 #define VINTAGE_GOLD	((colour){ 42, 48, 41 })
 
 #define BLACK		((colour){ 0, 0, 0 })
+#define WHITE		((colour){ 255, 255, 255 })
 #define RED		((colour){ 255, 0, 0 })
 #define GREEN		((colour){ 0, 255, 0 })
 #define BLUE		((colour){ 0, 0, 255 })
+
+static inline colour colour_make( int r, int g, int b )
+{
+	uint8_t _r, _g, _b;
+	colour c;
+
+	/* TODO: CLAMP */
+	_r = r;
+	_g = g;
+	_b = b;
+
+	c.r = _r;
+	c.g = _g;
+	c.b = _b;
+
+	return c;
+}
 
 static inline void colour_to_float( colour src, float *dest )
 {
