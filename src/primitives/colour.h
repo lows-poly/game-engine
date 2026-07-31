ef158b2cb291758af660f637bfef9166954e7c67
FjordPolaris <yorushika.lyrah@gmail.com>
2026-07-31 16:58:14 +0700

    Combine colour and colourf

--- src/primitives/colour.h
+++ src/primitives/colour.h
@@ -3,12 +3,21 @@
 
 #include <stdint.h>
 
+#include "mathx.h"
+
 typedef struct colour {
 	union {
-		struct { uint8_t r, g, b, a; };
+		struct { uint8_t r, g, b; };
 		uint8_t raw[3];
 	};
 } colour;
+
+typedef struct colourf {
+	union {
+		struct { float r, g, b, a; };
+		float raw[4];
+	};
+} colourf;
 
 #define VINTAGE_GOLD	((colour){ 42, 48, 41 })
 
@@ -17,22 +26,29 @@ typedef struct colour {
 #define RED		((colour){ 255, 0, 0 })
 #define GREEN		((colour){ 0, 255, 0 })
 #define BLUE		((colour){ 0, 0, 255 })
+#define CYAN            ((colour){ 0, 150, 150 })
 
 static inline colour colour_make( int r, int g, int b )
 {
-	uint8_t _r, _g, _b;
 	colour c;
 
-	/* TODO: CLAMP */
-	_r = r;
-	_g = g;
-	_b = b;
-
-	c.r = _r;
-	c.g = _g;
-	c.b = _b;
+	c.r = (uint8_t)clamp( r, 0, 255 );
+	c.g = (uint8_t)clamp( g, 0, 255 );
+	c.b = (uint8_t)clamp( b, 0, 255 );
 
 	return c;
+}
+
+static inline colourf colourf_make( colour c )
+{
+	colourf cf;
+
+	cf.r = c.r / 255.0f;
+	cf.g = c.g / 255.0f;
+	cf.b = c.b / 255.0f;
+	cf.a = 1.0f;
+
+	return cf;
 }
 
 static inline void colour_to_float( colour src, float *dest )
