3d8e9fb137bfdc804c45d90e68e3df63f26b0d8f
FjordPolaris <yorushika.lyrah@gmail.com>
2026-08-12 03:52:56 +0700

    Add 2D renderer

---
+++ src/renderer/renderer_2d.c
@@ -0,0 +1,111 @@
+#include <errno.h>
+
+#include "renderer_2d.h"
+#include "renderer.h"
+#include "log.h"
+
+static int set_projection( struct renderer_2d *r, int width, int height )
+{
+	mat4 proj;
+
+	if ( width <= 0 || height <= 0 ) {
+		pr_warn("FAILED TO SET 2D PROJECTION: INVALID ARGUMENTS\n");
+		return -EINVAL;
+	}
+
+	/*
+	 * PIXEL COORDINATES:
+	 * LEFT   = 0
+	 * RIGHT  = WIDTH
+	 * BOTTOM = HEIGHT
+	 * TOP    = 0
+	 */
+	mat4_ortho( 0.0f, (float)width, (float)height, 0.0f, -1.0f, 1.0f, proj );
+
+	shader_use( &r->shader );
+	if ( !shader_set_mat4( &r->shader, "u_projection", proj ) ) {
+		pr_warn("FAILED TO SET 2D PROJECTION UNIFORM\n");
+		return -EINVAL;
+	}
+
+	r->width = width;
+	r->height = height;
+
+	pr_info("SUCCESSFULLY SET 2D PROJECTION\n");
+	return 0;
+}
+
+/*
+ * renderer_2d_init() - Init shader 2d preset and set projection.
+ * @renderer_2d struct
+ * @width
+ * @height
+ *
+ * return: 0 on success, failure otherwise.
+ */
+int renderer_2d_init( struct renderer_2d *r, int width, int height )
+{
+	if ( !r || width <= 0 || height <= 0 ) {
+		pr_warn("FAILED TO INIT 2D RENDERER\n");
+
+		if ( !r )
+			pr_warn("RENDERER_2D IS NULL\n");
+		else
+			pr_warn("WIDTH, HEIGHT IS EXPECTED TO BE ABOVE ZERO\n");
+
+		return -EINVAL;
+	}
+
+	if ( shader_init_preset( &r->shader, SHADER_PRIMITIVE_2D ) != 0 ) {
+		pr_warn("FAILED TO INIT 2D RENDERER\n");
+		return -ENOMEM;
+	}
+
+	return set_projection( r, width, height );
+}
+
+/* TODO */
+void renderer_2d_set_shader( struct renderer_2d *r, struct shader *shader )
+{
+	if ( !r || !shader )
+		return;
+}
+
+void renderer_2d_update( struct renderer_2d *r, const struct window *w )
+{
+	if ( !w->resized )
+		return;
+
+	set_projection( r, w->width, w->height );
+}
+
+void renderer_2d_draw( struct renderer_2d *r, struct shape2d *shape )
+{
+	float colour_arr[4];
+	colour_to_arr( shape->colour, colour_arr );
+
+	shader_use( &r->shader );
+	shader_set_vec2( &r->shader, "u_pos", shape->pos );
+	shader_set_vec2( &r->shader, "u_scale", shape->scale );
+	shader_set_4f( &r->shader, "u_colour", colour_arr );
+	renderer_draw_mesh( &shape->mesh, &r->shader );
+}
+
+int renderer_2d_resize( struct renderer_2d *r, int width, int height )
+{
+	if ( width <= 0 || height <= 0 )
+		return -EINVAL;
+
+	return set_projection( r, width, height );
+}
+
+void renderer_2d_destroy( struct renderer_2d *r )
+{
+	if ( !r )
+		return;
+
+	shader_destroy( &r->shader );
+
+	r->width = 0;
+	r->height = 0;
+}
---
+++ src/renderer/renderer_2d.h
@@ -0,0 +1,32 @@
+#ifndef RENDERER_2D_H
+#define RENDERER_2D_H
+
+#include <stdbool.h>
+
+#include "graphics/shader/shader.h"
+#include "window/window.h"
+#include "primitives/shape2d.h"
+#include "primitives/colour.h"
+
+struct renderer_2d {
+	struct shader shader;
+	int width;
+	int height;
+};
+
+/*
+ * renderer_2d_init() - Init shader 2d preset and set projection.
+ * @renderer_2d struct
+ * @width
+ * @height
+ *
+ * return: 0 on success, failure otherwise.
+ */
+int renderer_2d_init( struct renderer_2d *r, int width, int height );
+void renderer_2d_set_shader( struct renderer_2d *r, struct shader *s );
+void renderer_2d_update( struct renderer_2d *r, const struct window *w );
+void renderer_2d_draw( struct renderer_2d *r, struct shape2d *shape );
+int renderer_2d_resize( struct renderer_2d *r, int width, int height );
+void renderer_2d_destroy( struct renderer_2d *r );
+
+#endif
