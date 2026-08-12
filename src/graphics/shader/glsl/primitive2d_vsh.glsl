bdb6c31b43b7a338e96acd16a14d874479125a8e
FjordPolaris <yorushika.lyrah@gmail.com>
2026-08-12 22:50:59 +0700

    Replace scale and position uniform into model uniform.

--- src/graphics/shader/glsl/primitive2d_vsh.glsl
+++ src/graphics/shader/glsl/primitive2d_vsh.glsl
@@ -3,11 +3,9 @@
 layout ( location = 0 ) in vec2 a_pos;
 
 uniform mat4 u_projection;
-uniform vec2 u_pos;
-uniform vec2 u_scale;
+uniform mat4 u_model;
 
 void main ()
 {
-	vec2 world_pos = a_pos * u_scale + u_pos;
-	gl_Position = u_projection * vec4( world_pos, 0.0, 1.0 );
+	gl_Position = u_projection * u_model * vec4( a_pos, 0.0, 1.0 );
 }
