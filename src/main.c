e5726fcb1b06f9ec70bd0582aefb1bf5db0d35f4
lowspoly <lowspoly@gmail.com>
2026-09-02 17:31:19 +0700

    Change demo to stars simulation

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "app/app.h"
 #include "window/window.h"
@@ -11,63 +12,14 @@
 #include "primitives/shape2d.h"
 #include "primitives/colour.h"
 
+#include "mathx.h"
 #include "log.h"
 
 #define WINDOW_TITLE         "ENGINE"
 #define WINDOW_WIDTH         800
 #define WINDOW_HEIGHT        600
 
-#define SPEED                200.0f
-
-#define MAX_BULLETS          3
-
-static void create_bullet( struct shape2d *arr, int *count, vec2 pos, float tri_height )
-{
-	bool ok;
-
-	if ( (*count) >= MAX_BULLETS )
-		return;
-
-	ok = shape2d_create( &arr[*count], SHAPE2D_RECTANGLE, pos.x,
-	                     pos.y - tri_height * 0.5f, 5.0f, 30.0f );
-
-	if ( !ok )
-		return;
-
-	shape2d_set_colour( &arr[*count], BLUE );
-	(*count)++;
-}
-
-static void draw_bullet( struct shape2d *arr, int count, struct renderer_2d *r )
-{
-	int i;
-
-	for ( i = 0; i < count; i++ )
-		renderer_2d_draw_shape( r, &arr[i] );
-}
-
-static void update_bullet( struct shape2d *arr, int *b_count, float speed )
-{
-	int i;
-	struct shape2d *bullet;
-
-	for ( i = 0; i < *b_count; i++ ) {
-		bullet = &arr[i];
-
-		if ( !bullet )
-			continue;
-
-		shape2d_move( bullet, 0.0f, -speed );
-
-		if ( bullet->pos.y + bullet->scale.y <= 0 ) {
-			shape2d_destroy( bullet );
-			arr[i] = arr[*b_count - 1];
-			(*b_count)--;
-			i--;
-		}
-		printf("BULLET: %p Y-POS (%.2f)\n", bullet , bullet->pos.y);
-	}
-}
+#define MAX_STARS          256
 
 int main( int argc, char *argv[] )
 {
@@ -75,10 +27,11 @@ int main( int argc, char *argv[] )
 	struct shape2d tri;
 	struct renderer_2d renderer;
 
-	struct shape2d bullets[MAX_BULLETS];
-	int bullet_count;
+	struct shape2d stars[MAX_STARS];
+	int start_count;
+	int i;
 
-	float dt, velocity;
+	float dt;
 
 	/*
 	 * APP SETUP
@@ -87,17 +40,21 @@ int main( int argc, char *argv[] )
 	if ( !app_init( &app, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, argv[0] ) )
 		return EXIT_FAILURE;
 
-	/* SHAPE SETUP */
-	if ( !shape2d_create( &tri, SHAPE2D_TRIANGLE, 400.0f, 100.0f, 40.0f, 40.0f ) )
-		return EXIT_FAILURE;
-
-	shape2d_set_colour( &tri, BLUE );
-
 	/* RENDERER SETUP */
 	if ( renderer_2d_init( &renderer, app.win.width, app.win.height ) != 0 )
 		return EXIT_FAILURE;
 
-	bullet_count = 0;
+	srand(time(NULL));
+	shape2d_set_default_colour( WHITE );
+
+	for ( i = 0; i < MAX_STARS; i++ ) {
+		vec2 pos = { randf( 0.1f, (float)WINDOW_WIDTH ),
+		             randf( 0.1f, (float)WINDOW_HEIGHT) };
+		vec2 scale = { randf( 0.1f, 2.5f ), randf( 0.1f, 2.5f ) };
+		shape2d_create( &stars[i], SHAPE2D_RECTANGLE, randf( 0.1f,
+		                (float)WINDOW_WIDTH ), randf( 0.1f, (float)WINDOW_HEIGHT ),
+		                randf( 1.0f, 2.5f ), randf( 1.0f, 2.5f ) );
+	}
 
 	while ( app.running ) {
 		renderer_begin_frame( BLACK );
@@ -107,53 +64,15 @@ int main( int argc, char *argv[] )
 			app_stop( &app );
 
 		dt = (float)app.time.delta_time;
-		velocity = SPEED * dt;
 
-		/* POSITIONING-CONTROLS */
-		if ( key_down( &app.input, KEY_A ) ) {
-			shape2d_move( &tri, -velocity, 0.0f );
-			printf("PLR_POS: (%.2f, %.2f)\n", tri.pos.x, tri.pos.y);
+		
+		for ( i = 0; i < MAX_STARS; i++ ) {
+			renderer_2d_draw_shape( &renderer, &stars[i] );
 		}
-
-		if ( key_down( &app.input, KEY_D ) ) {
-			shape2d_move( &tri, velocity, 0.0f );
-			printf("PLR_POS: (%.2f, %.2f)\n", tri.pos.x, tri.pos.y);
-		}
-
-		if ( key_down( &app.input, KEY_S ) ) {
-			shape2d_move( &tri, 0.0f, velocity );
-			printf("PLR_POS: (%.2f, %.2f)\n", tri.pos.x, tri.pos.y);
-		}
-
-		if ( key_down( &app.input, KEY_W ) ) {
-			shape2d_move( &tri, 0.0f, -velocity );
-			printf("PLR_POS: (%.2f, %.2f)\n", tri.pos.x, tri.pos.y);
-		}
-
-		/* ROTATIONAL-CONTROLS */
-		if ( key_down( &app.input, KEY_LEFT ) ) {
-			shape2d_rotate( &tri, -5.0f * dt );
-			printf("PLR_ROT: %.1f\n", tri.rotation);
-		}
-
-		if ( key_down( &app.input, KEY_RIGHT ) ) {
-			shape2d_rotate( &tri, 5.0f * dt );
-			printf("PLR_ROT: %.1f\n", tri.rotation);
-		}
-
-		/* BULLET */
-		if ( key_pressed( &app.input, KEY_ENTER ) )
-			create_bullet( bullets, &bullet_count, tri.pos, tri.scale.y );
-
-		update_bullet( bullets, &bullet_count, 700.0f * dt );
-		draw_bullet( bullets, bullet_count, &renderer );
-
-		renderer_2d_draw_shape( &renderer, &tri );
 
 		app_update( &app );
 	}
 
-	shape2d_destroy( &tri );
 	renderer_2d_destroy( &renderer );
 	app_shutdown( &app );
 
