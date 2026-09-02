deadba65eaac8f8246b96e9ed0d65e8bd7bdbe64
lowspoly <lowspoly@gmail.com>
2026-09-02 17:30:53 +0700

    Add randf()

--- include/mathx.h
+++ include/mathx.h
@@ -1,6 +1,8 @@
 #ifndef MATHX_H
 #define MATHX_H
 
+#include <stdlib.h>
+
 #define ENGINE_PI	3.14159265358979323846
 
 static inline float clampf( float x, float lo, float hi )
@@ -29,6 +31,14 @@ static inline int clamp( int x, int lo, int hi )
 		r = hi;
 
 	return r;
+}
+
+static inline float randf( float min, float max )
+{
+	float scale;
+	
+	scale = (float)rand() / (float)RAND_MAX;
+	return min + scale * (max - min);
 }
 
 #endif
